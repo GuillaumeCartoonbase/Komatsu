8801321195a6bbbad0c4b74b61edc5c596478ab8
guillaume <guillaume@MBP12.local>
2024-06-03 11:31:57 +0200

    add events handler

--- src/rive.js
+++ src/rive.js
@@ -18,3 +18,46 @@ window.addEventListener(
 	},
 	false
 );
+
+// Get Events
+const eventFire = (riveEvent) => {
+	const eventData = riveEvent.data;
+	const eventName = eventData.name;
+	const eventProperties = eventData.properties;
+
+	// Event logger
+	console.log(
+		"",
+		"event name:",
+		eventName,
+		"\n",
+		"event properties:",
+		eventProperties
+	);
+
+	switch (eventName) {
+		case "layer 1":
+			console.log("LAYER ONE");
+			break;
+		case "layer 2":
+			console.log("LAYER TWO");
+			break;
+		case "layer 3":
+			console.log("LAYER THREE");
+			break;
+
+		// Change pointer when hovering action
+		case "pointerEnter":
+			document.body.style.cursor = "pointer";
+			break;
+		case "pointerExit":
+			document.body.style.cursor = "auto";
+			break;
+
+		default:
+			console.log("Unhandled event:", eventName, "\n", riveEvent);
+			break;
+	}
+};
+
+riveInstance.on(rive.EventType.RiveEvent, eventFire);
